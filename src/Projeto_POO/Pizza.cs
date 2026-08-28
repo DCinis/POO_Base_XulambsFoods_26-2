4468731da0b822b63bef7e04ef3207d0238468ef
Danylo <danyloteixeira82@gmail.com>
2026-08-28 20:05:19 -0300

    supostamente terminada

--- src/Projeto_POO/Pizza.cs
+++ src/Projeto_POO/Pizza.cs
@@ -9,11 +9,13 @@ namespace XulambsFoods
         // ENTENDER O PROBLEMA!!!
         //Regra 0 -- não entre em pânico
         //Regra 1 -- não viaje
-        int maxIngredientes;
-        double precoBase;
-        int quantIngredientes;
-        double valorPorAdicional;
-        string descricao;
+        private const int MaxIngredientes = 8;
+        private const double PrecoBase = 29;
+        private const double ValorPorAdicional = 5;
+
+        
+        private int quantIngredientes;
+        private string descricao;
         public Pizza()
         {
             Init(0);
@@ -22,29 +24,27 @@ namespace XulambsFoods
         {
             Init(q);
         }
-        void Init(int q)
+        private void Init(int q)
         {
             descricao = "Pizza padrão";
-            precoBase = 29;
             quantIngredientes = AdicionarIngredientes(q);
-            valorPorAdicional = 5;
         }
-        double ValorAdicionais()
+        private double ValorAdicionais()
         {
-            return valorPorAdicional * quantIngredientes;
+            return ValorPorAdicional * quantIngredientes;
         }
-        string ModificarDescricao()
+        private string ModificarDescricao()
         {
             return $"Pizza com {quantIngredientes} adicionais";
         }
-        bool PodeAdicionar(int q)
+        private bool PodeAdicionar(int q)
         {
             if (quantIngredientes + q <= 8 && q > 0) return true;
             return false;
         }
-        public double CalcularValorFinal()
+        private double CalcularValorFinal()
         {
-            return precoBase + ValorAdicionais();
+            return PrecoBase + ValorAdicionais();
         }
         public int AdicionarIngredientes(int q)
         {
